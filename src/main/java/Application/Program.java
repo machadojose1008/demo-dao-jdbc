cc0eeec81aea01a8549b6660255623b58879088f
MachadoJose <jgbm81@Hotmail.com>
2025-04-25 14:49:20 -0400

    Seller entity class

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -1,10 +1,15 @@
 package Application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
+        Seller seller = new Seller(21,"bob","bob@mail.com", LocalDate.now(),3000.0,obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
