757c089a0eace85fbd417319ef82bfc13274088d
MachadoJose <jgbm81@Hotmail.com>
2025-04-25 15:03:18 -0400

    SellerDaoJDBC and DaoFactory

--- src/main/java/Application/Program.java
+++ src/main/java/Application/Program.java
@@ -1,5 +1,7 @@
 package Application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -9,6 +11,9 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
         Seller seller = new Seller(21,"bob","bob@mail.com", LocalDate.now(),3000.0,obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(obj);
         System.out.println(seller);
     }
