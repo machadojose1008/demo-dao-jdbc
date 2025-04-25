bdf62c04312c937ee12fe3f0d20bb955d4879037
MachadoJose <jgbm81@Hotmail.com>
2025-04-25 14:28:29 -0400

    Basic Structure

--- src/main/java/DB/DB.java
+++ src/main/java/DB/DB.java
@@ -1,4 +1,4 @@
-package db;
+package DB;
 
 import java.io.FileInputStream;
 import java.io.IOException;
--- src/main/java/DB/DbException.java
+++ src/main/java/DB/DbException.java
@@ -1,4 +1,4 @@
-package db;
+package DB;
 
 public class DbException extends RuntimeException{
 
